f83dee147626be6f8a5f6f565eedf1cd9c55e889
Gyanranjan Patra 🤖 <153155648+Gyannnnn@users.noreply.github.com>
2024-06-09 15:22:58 +0530

    Update Index.js

--- Server/Index.js
+++ Server/Index.js
@@ -6,7 +6,7 @@ const todoModel = require('./Models/Todo');
 const app = express();
 app.use(cors(
     {
-        origin: ["https://deploy-mern-frontend.vercel.app"],
+        origin: ["https://ecell-taskontend.vercel.app"],
         methods: ["POST", "GET"],
         credentials: true
     }
